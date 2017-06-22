8a8c57dfc0ee0330b74313a976f7aafe3fc0d817
y77cao <y77cao@uwaterloo.ca>
2017-06-22 00:05:23 -0400

    Write issue fixed yeeeeeeeh

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -6,6 +6,18 @@ app.factory('Stickers', ['$resource', function($resource) {
     });
   }]);
 
+app.controller('parentController', ['$scope', 'Stickers', function($scope, Stickers) {
+  $scope.stickers = Stickers.query();
+  $scope.save = function(){
+      if(!$scope.newSticker || $scope.newSticker.length < 1) return;
+      var stk = new Stickers({name: 'tester', stkContent: $scope.newSticker, uniID:'sjfgwauf'});
+    stk.$save(function(){
+     $scope.stickers.push(stk);
+         $scope.newSticker = '';
+        });  
+      }    
+}]);
+
 app.controller('stickerController', ['$scope', 'Stickers', function($scope, Stickers) {
   //default values
   $scope.mainPage = true;
@@ -14,113 +26,13 @@ app.controller('stickerController', ['$scope', 'Stickers', function($scope, Stic
   $scope.deleting = [];
   $scope.writeDetail = false;
   $scope.searchDetail = false;
-  $scope.stickers = Stickers.query();
-/*
-Stickers.create(
-   {name: 'ehhhh',
-   uniID: 'dogPzIz8',
-   stkContent: 'Im glad to hear it!'}, function(err, todo){
-     if(err) console.log(err);
-     else console.log(todo);
-   });*/
-
-  /*[
-  {name: 'yc',
-   uniID: '23TplPdS',
-   stkContent: 'This is a test',
-   stkTime:''},
-   
-   {name: 'lalala',
-   uniID: '46Juzcyx',
-   stkContent: 'Is it working? Cool, you are done then! Is NOT working? take a look at the full repository.',
-   stkTime:''},
-   
-   {name: 'y77cao',
-   uniID: 'dBvJIh-H',
-   stkContent: 'I appreciate your bottom-up approach to teaching. Start with the smallest building blocks and then assemble them into something meaningful.',
-   stkTime:''},
-   
-   {name: 'loo',
-   uniID: '2WEKaVNO',
-   stkContent: 'I think you forgot to include the step of installing mongoose. I had to ',
-   stkTime:''},
-   
-   {name: 'ehhhh',
-   uniID: 'dogPzIz8',
-   stkContent: 'Im glad to hear it!', 
-   stkTime:''},
-   
-   {name: 'WTF',
-   uniID: 'nYrnfYEv',
-   stkContent: 'Amazingly short non-sequential url-friendly unique id generator. ShortId creates amazingly short non-sequential url-friendly unique ids.',
-   stkTime:''},
-   
-   {name: 'Alice',
-   uniID: 'a4vhAoFG',
-   stkContent: 'Short one here',
-   stkTime:''},
-
-   {name: 'yc',
-   uniID: '23TplPdS',
-   stkContent: 'This is a test',
-   stkTime:''},
-   
-   {name: 'lalala',
-   uniID: '46Juzcyx',
-   stkContent: 'Is it working? Cool, you are done then! Is NOT working? take a look at the full repository.',
-   stkTime:''},
-   
-   {name: 'y77cao',
-   uniID: 'dBvJIh-H',
-   stkContent: 'I appreciate your bottom-up approach to teaching. Start with the smallest building blocks and then assemble them into something meaningful.',
-   stkTime:''},
-   
-   {name: 'loo',
-   uniID: '2WEKaVNO',
-   stkContent: 'I think you forgot to include the step of installing mongoose. I had to ',
-   stkTime:''},
-   
-   {name: 'ehhhh',
-   uniID: 'dogPzIz8',
-   stkContent: 'Im glad to hear it!', 
-   stkTime:''},
-   
-   {name: 'WTF',
-   uniID: 'nYrnfYEv',
-   stkContent: 'Amazingly short non-sequential url-friendly unique id generator. ShortId creates amazingly short non-sequential url-friendly unique ids.',
-   stkTime:''},
-   
-   {name: 'Alice',
-   uniID: 'a4vhAoFG',
-   stkContent: 'Short one here',
-   stkTime:''},
-
-   {name: 'WTF',
-   uniID: 'nYrnfYEv',
-   stkContent: 'Amazingly short non-sequential url-friendly unique id generator. ShortId creates amazingly short non-sequential url-friendly unique ids.',
-   stkTime:''},
-]; */
+ // $scope.stickers = Stickers.query();
 
   $scope.colorSet = ["#9fdbfc", "#fcbfbf", "#fceebf"];
 /*
   $scope.randomnizeColor = function() {
     $scope.bgColor = colorSet[Math.floor(Math.random() * $scope.colorSet.length)];
   } */
-
-  $scope.save = function(){
-    console.log("Save clicked"); 
-      if(!$scope.newSticker || $scope.newSticker.length < 1) {
-        console.log("User input NULL");
-        return;
-      }
-      var stk = new Stickers({name: 'tester', stkContent: 'shit', uniID:'fgsrgegr'});
-      console.log(stk);
-    stk.$save(function(){
-     $scope.stickers.push(stk);
-         $scope.newSticker = '';
-         console.log('Saved'); // clear textbox
-        });  
-      }    
     
   $scope.editDetail = function(index){
       $scope.editing[index] = angular.copy($scope.stickers[index]);
